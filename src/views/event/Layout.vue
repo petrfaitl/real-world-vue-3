e31598a02ded8702c346e1dc685a3fc07cd25383
Petr Faitl <pfaitl@gmail.com>
2022-07-24 21:38:10 +1200

    Remove Watcher

--- src/views/event/Layout.vue
+++ src/views/event/Layout.vue
@@ -11,16 +11,15 @@
       >
     </div>
     <h1 class="font-bold text-2xl my-2">{{ event.title }}</h1>
-    <div class="mb-3">
+    <div class="mb-3 flex gap-3 mx-auto">
       <router-link
         :to="{ name: 'EventDetails', params: { id } }"
-        v-if="$route.name === 'EventRegister' || $route.name === 'EventEdit'"
         class="font-bold text-vue-green hover:text-vue-green-dark"
         >Details</router-link
       >
+      |
       <router-link
         :to="{ name: 'EventRegister' }"
-        v-else
         class="font-bold text-vue-green hover:text-vue-green-dark"
         >Register</router-link
       >
@@ -35,7 +34,6 @@
 <script>
 import EventService from "@/services/EventService";
 import LoadingText from "@/components/LoadingText";
-import { watchEffect } from "vue";
 
 export default {
   name: "EventLayout",
@@ -48,9 +46,7 @@ export default {
   props: ["id"],
   async created() {
     try {
-      watchEffect(async () => {
-        this.event = await EventService.getEventDetails(this.id);
-      });
+      this.event = await EventService.getEventDetails(this.id);
     } catch (e) {
       console.log(e);
     }
